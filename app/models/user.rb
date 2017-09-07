5ac57ca51e6ad97ebadafb4bf139530c0c31a90a
Tadayuki Ri <zhaowei71lee@gmail.com>
2017-09-07 10:54:00 +0900

    finish account_activation mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
-	before_save { email.downcase! }
-	# before_save { self.email = email.downcase }も可
+	before_save   :downcase_email
+	before_create :create_activation_digest
 
 	validates :name, presence: true, length:{maximum:50}
 	validates :email, presence: true, length:{maximum:255}
@@ -18,29 +18,42 @@ class User < ApplicationRecord
  	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    	BCrypt::Password.create(string, cost: cost)
 	end
 
  	# ランダムなトークンを返す
  	def User.new_token
-  	SecureRandom.urlsafe_base64
+  		SecureRandom.urlsafe_base64
  	end
 
 	# 永続セッションのためにユーザーをデータベースに記憶する
 	def remember
-  	self.remember_token = User.new_token
-  	update_attribute(:remember_digest, User.digest(remember_token))
+  		self.remember_token = User.new_token
+  		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
 	# 渡されたトークンがダイジェストと一致したらtrueを返す
 	def authenticated?(remember_token)
 		return false if remember_digest.nil?
-  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
 	# ユーザーのログイン情報を破棄する
 	def forget
-  	update_attribute(:remember_digest, nil)
+  		update_attribute(:remember_digest, nil)
  	end
+
+ 	private
+
+    	# メールアドレスをすべて小文字にする
+    	def downcase_email
+     		self.email = email.downcase
+    	end
+
+    	# 有効化トークンとダイジェストを作成および代入する
+    	def create_activation_digest
+      		self.activation_token  = User.new_token
+      		self.activation_digest = User.digest(activation_token)
+    	end
 
 end

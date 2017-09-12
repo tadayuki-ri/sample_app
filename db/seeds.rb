a960d05442d2b89e0e52801992649041e0a08b1f
Tadayuki Ri <zhaowei71lee@gmail.com>
2017-09-12 15:47:17 +0900

    finish basic following-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -26,8 +22,17 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) } # userはfollowingたち(followedの集まり)をフォローする
+followers.each { |follower| follower.follow(user) } # userはfollowesたち(followerの集まり)にフォローされる
